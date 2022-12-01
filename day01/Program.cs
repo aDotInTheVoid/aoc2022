80ab55156b857702f401fa24ce03950532442612
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2022-12-01 14:48:31 +0000

    Day 1 Part 1

--- day01/Program.cs
+++ day01/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string input = System.IO.File.ReadAllText("input.txt");
+
+string[] elves = input.Split("\n\n");
+
+
+long sumElf(string[] sums)
+{
+    long sum = 0;
+    foreach (string s in sums)
+    {
+        if (s == "")
+            continue;
+        sum += Int64.Parse(s);
+    }
+    return sum;
+}
+
+long maxSum = elves.Select(x => sumElf(x.Split("\n"))).Max();
+
+Console.WriteLine(maxSum);
--- notes/day01-csharp.md
+++ notes/day01-csharp.md
@@ -1,4 +1,17 @@
+# Setup
+
 sudo apt install dotnet6
 dotnet new console -o day01
 cd day01
 dotnet new gitignore
+dotnet run
+
+Editor: https://github.com/OmniSharp/omnisharp-vscode
+
+# Reading a file
+
+https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/file-system/how-to-read-from-a-text-file
+
+https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/types/how-to-convert-a-string-to-a-number
+
+https://learn.microsoft.com/en-us/dotnet/csharp/how-to/parse-strings-using-split
