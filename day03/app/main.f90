a9217919de00337e3029e4efe82f955b1f50972b
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2022-12-03 14:54:25 +0000

    Day 2 Read File

--- day03/app/main.f90
+++ day03/app/main.f90
@@ -1,7 +1,25 @@
 program main
-  use day03, only: say_hello
-  use stdlib_io, only: getline
-  implicit none
+   ! use day03, only: say_hello
+   use, intrinsic :: iso_fortran_env, only: input_unit, output_unit
+   use stdlib_io, only: getline
+   implicit none
 
-  call say_hello()
+   character(len=:), allocatable :: line
+   integer :: stat
+   integer :: input_len
+
+   open(unit=1, file="input.txt")
+
+   do
+      call getline(1, line, stat)
+      if (stat /= 0) then
+         exit
+      end if
+      input_len = LEN(line)
+      write(*,*) line
+      write(*,*) input_len
+   end do
+
+   close(unit=1)
+
 end program main
