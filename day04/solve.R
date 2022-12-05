c22606411148e2e27666ab92c5e7f597c2885be1
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2022-12-05 01:01:12 +0000

    Day 4 part 1

---
+++ day04/solve.R
@@ -0,0 +1,23 @@
+input <- readLines("input.txt")
+
+sc1 <- 0
+
+for (i in 1:length(input)) {
+    intervals <- strsplit(input[i], ",")[[1]] 
+
+    i1 <- strsplit(intervals[1], "-")[[1]]
+    i2 <- strsplit(intervals[2], "-")[[1]]
+
+    i1_lo <- as.numeric(i1[1])
+    i1_hi <- as.numeric(i1[2])
+    i2_lo <- as.numeric(i2[1])
+    i2_hi <- as.numeric(i2[2])
+
+    if (i1_lo <= i2_lo && i1_hi >= i2_hi) {
+        sc1 <- sc1 + 1
+    } else if (i2_lo <= i1_lo && i2_hi >= i1_hi) {
+        sc1 <- sc1 + 1
+    }
+}
+
+print(sc1) # 573
