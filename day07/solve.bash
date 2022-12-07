627098f1297a4c46082601ec729d0f5e10223195
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2022-12-07 06:13:08 +0000

    Fix Q2 (RTFQ)

--- day07/solve.bash
+++ day07/solve.bash
@@ -11,12 +11,10 @@ rm -rf sizes.txt
 for dirname in $(find tmp -type d)
 do
     sum=0
-    #echo "--- $dirname ---"
     for filename in $(find $dirname -type f)
     do
         n=$(cat $filename)
         sum=$((sum + n))
-        # echo "$sum $filename $n"
     done
     echo $dirname $sum >> sizes.txt
 
@@ -29,18 +27,21 @@ done
 
 echo "Q1: $q1sum"
 
+sort sizes.txt -k 2 -n > szsort.txt
 
-sort sizes.txt -k 2 -n | while IFS= read -r dat
+totsz=$(tail -n1 szsort.txt | cut -d' ' -f2)
+needed=$((totsz - 40000000))
+
+echo $needed
+
+cat szsort.txt | while IFS= read -r dat
 do
     sz=$(cut -d' ' -f2 <<< $dat)
-    #echo "dat=$dat, sz=$sz"
-    if [[ $sz -gt 8381164 ]]
+    if [[ $sz -ge $needed ]] # 
     then
         echo "Q2: $sz"
         break
     fi
 done
-
-sort sizes.txt -k 2 -n > szsort.txt
 
 # rm sizes.txt
