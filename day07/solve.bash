87ffe6e96d21229207ac43300ceabcf964a72fd5
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2022-12-07 06:18:15 +0000

    Cleanup day7

--- day07/solve.bash
+++ day07/solve.bash
@@ -1,13 +1,31 @@
 #!/bin/bash
 set -eou pipefail
 
-./gen.bash > mkfs.bash
+rm -rf ./tmp ./mkfs.bash ./sizes.txt ./szsort.txt
+mkdir tmp
+
+echo "#!/bin/bash" >> mkfs.bash
+echo "set -eou pipefail" >> mkfs.bash
+
+while read p; do
+  if [[ $p =~ "\$ cd" ]]; then
+    p=${p/\$ /}
+    echo ${p/\//tmp} >> mkfs.bash
+  elif [[ $p =~ "dir" ]]; then
+    echo "mkdir ${p/dir /}" >> mkfs.bash
+  elif [[ $p =~ " ls" ]]; then 
+    :
+  else
+    sz=$(cut -d' ' -f1 <<< $p)
+    name=$(cut -d' ' -f2 <<< $p)
+    echo "echo $sz > $name" >> mkfs.bash
+  fi
+done <input.txt
+
+chmod +x mkfs.bash
 ./mkfs.bash
 
 q1sum=0
-
-rm -rf sizes.txt
-
 for dirname in $(find tmp -type d)
 do
     sum=0
@@ -24,15 +42,12 @@ do
     fi
 
 done
-
 echo "Q1: $q1sum"
 
 sort sizes.txt -k 2 -n > szsort.txt
 
 totsz=$(tail -n1 szsort.txt | cut -d' ' -f2)
 needed=$((totsz - 40000000))
-
-echo $needed
 
 cat szsort.txt | while IFS= read -r dat
 do
@@ -43,5 +58,3 @@ do
         break
     fi
 done
-
-# rm sizes.txt
